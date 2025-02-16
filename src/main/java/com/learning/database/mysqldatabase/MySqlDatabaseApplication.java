7a21dca614ab84e0cd293a7f8cf09a9f1e8f72ce
Ashok B <ashokloy@gmail.com>
2025-02-16 15:30:06 +0530

    modified the main class and add git ignore file

--- src/main/java/com/learning/database/mysqldatabase/MySqlDatabaseApplication.java
+++ src/main/java/com/learning/database/mysqldatabase/MySqlDatabaseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MysqldatabaseApplication {
+public class MySqlDatabaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MysqldatabaseApplication.class, args);
+		SpringApplication.run(MySqlDatabaseApplication.class, args);
 	}
 
 }
